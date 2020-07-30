c8432dea240ba922931e744d91d2c7889b255944
Ilu_ha <ziluha491@gmail.com>
2020-07-30 19:31:20 +0300

    add 2.py

--- 2.py
+++ 2.py
@@ -4,3 +4,46 @@
 # размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и H, соответственно.
 # Для определения расхода ткани по каждому типу одежды использовать формулы: 
 # для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
+
+class Textil:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_c(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_j(self):
+        return self.height * 2 + 0.3
+
+    @property
+    def get_sq_full(self):
+        return str(f'Площадь общая ткани \n'
+                   f' {(self.width / 6.5 + 0.5) + (self.height * 2 + 0.3)}')
+
+
+class Coat(Textil):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_c = round(self.width / 6.5 + 0.5)
+
+    def __str__(self):
+        return f'Площадь на пальто {self.square_c}'
+
+
+class Jacket(Textil):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_j = round(self.height * 2 + 0.3)
+
+    def __str__(self):
+        return f'Площадь на костюм {self.square_j}'
+
+coat = Coat(2, 4)
+jacket = Jacket(1, 2)
+print(coat)
+print(jacket)
+print(coat.get_sq_full)
+print(jacket.get_sq_full)
+print(jacket.get_square_c())
+print(jacket.get_square_j())
